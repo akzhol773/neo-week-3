4fce8d37eef8c3599d0e71a5ff78811136bb4b9d
akzhol <akzholbek0096@gmail.com>
2024-01-09 13:13:24 +0000

    created a new project and added a README file

---
+++ src/main/java/com/akzhol/springbootneobis/SpringbootNeobisApplication.java
@@ -0,0 +1,13 @@
+package com.akzhol.springbootneobis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNeobisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootNeobisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akzhol/springbootneobis/SpringbootNeobisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akzhol.springbootneobis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNeobisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
